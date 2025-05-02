a360082f1210cd980e28fca3e1952688884d9c7b
arlaspresident <jankovic4901@gmail.com>
2025-05-02 16:18:58 +0200

    bytte url

--- register.js
+++ register.js
@@ -8,7 +8,7 @@ form.addEventListener('submit', async (e) => {
   const password = form.password.value;
 
   try {
-    const response = await fetch('http://localhost:3000/api/register', {
+    const response = await fetch('https://moment4-sw08.onrender.com', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ username, password })
--- script.js
+++ script.js
@@ -8,7 +8,7 @@ form.addEventListener('submit', async (e) => {
   const password = document.getElementById('password').value;
 
   try {
-    const response = await fetch('http://localhost:3000/api/login', {
+    const response = await fetch('https://moment4-sw08.onrender.com', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ username, password })
